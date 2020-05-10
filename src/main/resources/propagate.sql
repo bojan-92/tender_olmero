762ba68972fef31f2a551c9368d86004a39565d1
bojan.trifkovic <bojan.trifkovic@nortal.com>
2020-05-10 13:50:12 +0200

    Add propagate DB script

---
+++ src/main/resources/propagate.sql
@@ -0,0 +1,30 @@
+CREATE DATABASE tender_olmero;
+
+CREATE TABLE tender_olmero.offers (
+    id INT NOT NULL PRIMARY KEY auto_increment,
+    isAccepted INT,
+    tender_id INT,
+    bidder_id INT
+);
+
+CREATE TABLE tender_olmero.issuers (
+    id INT NOT NULL PRIMARY KEY auto_increment,
+	issuer_name VARCHAR(255)
+);
+
+CREATE TABLE tender_olmero.tenders (
+    id INT NOT NULL PRIMARY KEY auto_increment,
+	work_description VARCHAR(255),
+	issuer_id INT
+);
+
+CREATE TABLE tender_olmero.bidders (
+    id INT NOT NULL PRIMARY KEY auto_increment,
+	bidder_name VARCHAR(255)
+);
+
+INSERT INTO issuers (id, issuer_name) values (1, "issuer 1");
+INSERT INTO issuers (id, issuer_name) values (2, "issuer 2");
+
+INSERT INTO bidders (id, bidder_name) values (1, "bidder 1");
+INSERT INTO bidders (id, bidder_name) values (2, "bidder 2");
